d801ff237edad6d10e4114290cabde34a09c4823
Laputa0 <1737261850wang@gmail.com>
2025-08-21 08:40:09 +0000

    update

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -40,9 +40,9 @@ echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
 echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 
 #手动调整的插件
-if [ -n "$WRT_PACKAGE" ]; then
-	echo -e "$WRT_PACKAGE" >> ./.config
-fi
+# if [ -n "$WRT_PACKAGE" ]; then
+# 	echo -e "$WRT_PACKAGE" >> ./.config
+# fi
 
 #高通平台调整
 DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
