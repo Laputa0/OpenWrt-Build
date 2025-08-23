617c2356e783b42e995851248600250d39097a87
Laputa0 <1737261850wang@gmail.com>
2025-08-23 13:12:10 +0000

    update

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -25,16 +25,16 @@ PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 # fi
 
 #修改argon主题字体和颜色
-if [ -d *"luci-theme-argon"* ]; then
-	echo " "
+# if [ -d *"luci-theme-argon"* ]; then
+# 	echo " "
 
-	cd ./luci-theme-argon/
+# 	cd ./luci-theme-argon/
 
-	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+# 	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
+# 	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
 
-	cd $PKG_PATH && echo "theme-argon has been fixed!"
-fi
+# 	cd $PKG_PATH && echo "theme-argon has been fixed!"
+# fi
 
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
@@ -57,14 +57,14 @@ if [ -f "$NSS_PBUF" ]; then
 fi
 
 #修复TailScale配置文件冲突
-TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
-if [ -f "$TS_FILE" ]; then
-	echo " "
+# TS_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/tailscale/Makefile")
+# if [ -f "$TS_FILE" ]; then
+# 	echo " "
 
-	sed -i '/\/files/d' $TS_FILE
+# 	sed -i '/\/files/d' $TS_FILE
 
-	cd $PKG_PATH && echo "tailscale has been fixed!"
-fi
+# 	cd $PKG_PATH && echo "tailscale has been fixed!"
+# fi
 
 #修复Rust编译失败
 RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
